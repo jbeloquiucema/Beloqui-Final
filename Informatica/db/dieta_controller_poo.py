496e28f507568d34d23f54a4d2ae61b6ca98d92c
jbeloquiucema <134952934+jbeloquiucema@users.noreply.github.com>
2023-07-06 11:44:00 -0300

    Create dieta_controller_poo.py

---
+++ Informatica/db/dieta_controller_poo.py
@@ -0,0 +1,35 @@
+from db_dietas import get_db
+from clase_dietas import Dieta
+
+
+def insert_dieta(id, Restriction, Restriccion, USD):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "INSERT INTO dieta (id, Restriction, Restriccion, USD) \
+    VALUES ( ?, ?, ?, ?)"
+    cursor.execute(statement, [id, Restriction, Restriccion, USD])
+    db.commit()
+    return True
+
+def update_dieta(id, Restriction, Restriccion, USD):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "UPDATE dietas SET Restriiction = ?, Restriccion = ?, USD= ?\
+    WHERE id = ?"
+    cursor.execute(statement, [Restriction, Restriccion, USD, id])
+    db.commit()
+    return True
+
+
+def get_by_id(id):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "SELECT id, Restriccion, USD FROM dietas \
+    WHERE id = ?"
+    cursor.execute(statement, [id])
+    single_dieta = cursor.fetchone()
+    id = single_dieta[0]
+    Restriccion = single_dieta[2]
+    USD = single_dieta[3]
+    dieta = Dieta (id, Restriccion, USD) 
+    return dieta.serialize.details()
